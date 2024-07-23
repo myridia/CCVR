42ac5eb254bffef86c3352d3294e54d724ab13da
FuckyouMS6 <veto@myridia.com>
2024-07-23 11:35:43 +0700

    update make linux maker, but window does not yet works

---
+++ clean.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo  rm dist build -Rf 
--- make_linux_binary.sh
+++ make_linux_binary.sh
@@ -1,5 +1,15 @@
-# See https://myridia.com/dev_posts/view/3489 for more info 
-docker run -v "$(pwd):/src/" engineervix/pyinstaller-linux "pyinstaller -F main.py"
-docker run -v "$(pwd):/src/" engineervix/pyinstaller-linux
-sudo cp config.ini dist/
-sudo cp config.ini dist/linux
+#!/bin/sh
+# See https://myridia.com/dev_posts/view/3489 for more info
+
+docker run -v "$(pwd):/src/" batonogov/pyinstaller-linux "pyinstaller -F main.py"
+docker run -v "$(pwd):/src/" batonogov/pyinstaller-linux
+
+if [ -f config.ini ] #check if file exists
+then
+   echo "...copy config.ini to dist/config.ini"    
+   sudo cp config.ini dist/
+else
+   echo "...copy config_defaut.ini to dist/config.ini"
+   sudo cp config_default.ini dist/config.ini
+fi
+
