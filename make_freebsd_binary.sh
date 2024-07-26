3c5264163b20063ada696544c0d2fc4ac3d5d762
User Veto <veto@dev.lap>
2024-07-26 04:01:28 +0000

    makde freeBSD binary

---
+++ make_freebsd_binary.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+rm -rf dist 
+rm -rf build
+
+pyinstaller main.py -F -w -n ccvr_freebsd 
+
+if [ -f config.ini ] #check if file exists
+then
+   echo "...copy config.ini to dist/config.ini"    
+   sudo cp config.ini dist/
+else
+   echo "...copy config_defaut.ini to dist/config.ini"
+   sudo cp config_default.ini dist/config.ini
+fi
+
+
+
+if [ -d log_example ] #check if file exists
+then
+   echo "...copy log example"    
+   sudo cp -rf log_example dist/ 
+fi
+
+
+
+
+
+ 
+
